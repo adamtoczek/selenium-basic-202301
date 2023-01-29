cbb76a72e5a3de76534c9ee16f41b5548f4616c4
Adam Toczek <a.a.toczek@gmail.com>
2023-01-29 10:45:34 +0100

    form test continue

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,7 +2,10 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.openqa.selenium.*;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class FormTest extends TestBase{
     @Test
@@ -10,8 +13,19 @@ public class FormTest extends TestBase{
         driver.get("http://51.75.61.161:9102/form.php");
 
         WebElement firstName = driver.findElement(By.id("inputFirstName3"));
-
         firstName.sendKeys("Adam");
+
+        driver.findElement(By.id("inputLastName3")).sendKeys("Kwiatkowski");
+        driver.findElement(By.id("inputEmail3")).sendKeys("test@sii.pl");
+        driver.findElement(By.id("inputAge3")).sendKeys("12");
+        driver.findElement(By.cssSelector("*[name=\"gridRadiosExperience\"][value=\"7\"]")).click();
+
+        List<String> proffesions = Arrays.asList("automation tester", "manual tester");
+        List<WebElement> proffesionsLabels = driver.findElements(By.cssSelector("*[name=\"gridCheckboxProfession\"]+label"));
+        for (WebElement label : proffesionsLabels) {
+            if (proffesions.contains(label.getText().trim().toLowerCase()))
+                label.click();
+        }
 
         List<WebElement> gender = driver.findElements(By.name("gridRadiosSex"));
 
@@ -21,6 +35,9 @@ public class FormTest extends TestBase{
                 break;
             }
         }
+        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        assertEquals(driver.findElement(By.id("validator-message")).getText().trim(), "Form send with success");
+
 
         Thread.sleep(5000);
     }
