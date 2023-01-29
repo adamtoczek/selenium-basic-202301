a8e6164fc016b96f858f639675b044bdd537e91c
Adam Toczek <a.a.toczek@gmail.com>
2023-01-29 12:03:06 +0100

    form complete

---
+++ src/test/java/AlertTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class AlertTest extends TestBase{
+    @BeforeMethod
+    public void setupPage() {
+        driver.get("http://51.75.61.161:9102/alerts.php");
+    }
+
+    @Test
+    public void shouldAcceptAlert() {
+        driver.findElement(By.id("simple-alert")).click();
+        driver.switchTo().alert().accept();
+        assertEquals(driver.findElement(By.id("simple-alert-label")).getText(), "OK button pressed");
+    }
+
+    @Test
+    public void shouldFillPromptAlert() {
+        driver.findElement(By.id("prompt-alert")).click();
+        driver.switchTo().alert().sendKeys("Jan");
+        driver.switchTo().alert().accept();
+        assertEquals(driver.findElement(By.id("prompt-label")).getText(), "Hello Jan! How are you today?");
+    }
+
+    @Test
+    public void shouldDismissAlert() {
+        driver.findElement(By.id("confirm-alert")).click();
+        driver.switchTo().alert().dismiss();
+        assertEquals(driver.findElement(By.id("confirm-label")).getText(), "You pressed Cancel!");
+    }
+
+    //popup przegladarkowy Basic Auth nie da sie obsluzyc metodami do alertow
+    @Test
+    public void shouldAcceptBasicAuth() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Basic Auth")).click();
+        driver.switchTo().alert().sendKeys("admin\tadmin");
+        Thread.sleep(5000);
+    }
+
+
+
+}
